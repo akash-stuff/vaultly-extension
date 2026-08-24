8b43bbda408a2b929d4034f2d60dc7519847861c
akash-stuff <akashrioevil444@gmail.com>
2026-08-24 19:08:38 +0530

    Delete extension directory

--- extension/config.js
+++
@@ -1,3 +0,0 @@
-// Central place to configure the API base URL used by the extension.
-// Point this at your deployed backend in production.
-export const API_BASE_URL = 'https://vaultly-service.onrender.com/api';
